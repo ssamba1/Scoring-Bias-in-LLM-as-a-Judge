3ecc8ef41be9ce12aebee1f6d2b0f5c4aa8e4c33
Sricharan Samba <srisamba09@gmail.com>
2026-07-14 23:57:16 -0400

    Round 3: MIT license, Binder env, Docker 4-service, gitignore cleanup, remote cleanup, temp files gitignored

--- check_braces.py
+++
@@ -1,8 +0,0 @@
-with open('paper/camera_ready_full.tex', 'r') as f:
-    content = f.read()
-opens = content.count('{')
-closes = content.count('}')
-print(f'Open braces: {opens}, Close braces: {closes}, Diff: {opens-closes}')
-starts = content.count(r'\begin{')
-ends = content.count(r'\end{')
-print(f'\\begin{{}}: {starts}, \\end{{}}: {ends}, Diff: {starts-ends}')
